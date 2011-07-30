09f11492c3ecaf9bb8fb8da9c4b93a73a623d9ac
Dmitry Chernyak <dmi.chernyak@gmail.com>
2011-07-31 02:03:24 +0400

    Updated SkyCover stuff

--- getfresh.sh
+++ getfresh.sh
@@ -1,3 +1,6 @@
 wgets="wget --no-check-certificate"
-#$wgets https://github.com/skycover/scduply/tarball/master
+rm master
+$wgets https://github.com/skycover/scduply/tarball/master
+mv master skycover-scduply-latest.tar.gz
 $wgets https://github.com/skycover/scdw/tarball/master
+mv master skycover-scdw-latest.tar.gz
