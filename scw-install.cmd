17146249002253d0274257c5612555f2b537ac1e
Dmitry Chernyak <dmi.chernyak@gmail.com>
2014-08-24 00:22:08 +0400

    Two phase install with automatic cygwin download

--- scw-install.cmd
+++ scw-install.cmd
@@ -1,20 +1,17 @@
 set src=C:\Distrib\scwin
-if not exist %src%/setup.exe goto message
+set cygsetup=setup-x86.exe
+if defined ProgramFiles(x86) set cygsetup=setup-x86_64.exe
+if exist %src%/%cygsetup% goto cygok
+wget http://cygwin.com/%cygsetup%
+if not exist %src%/%cygsetup% goto message
+:cygok
 set dst=c:\cygwin
 set asm=%dst%\usr\local\src
-set packages="python,gnupg,gcc,librsync-devel,librsync1,wget,vim,ncftp,openssh,cron"
+set packages="python,gnupg,gcc,gcc-core,cyglsa,librsync-devel,librsync1,wget,vim,ncftp,openssh,cron"
 chdir %src%
 mkdir cygwin
 rem Use scw-install.cmd -L to install cygwin from local directory
-setup.exe %1 -l %src%\cygwin -R %dst% -q -P %packages%
-mkdir %asm%
-copy *.tar.gz %asm%
-copy scw-postinst.sh %asm%
-copy sysstate.* %asm%
-copy scdw.cmd %asm%
-chdir %asm%
-%dst%\bin\bash %asm%\scw-postinst.sh
-chdir %src%
+%cygsetup% %1 -l %src%\cygwin -R %dst% -q -P %packages%
 goto qt
 :message
 echo You should place ditributive to %src%
