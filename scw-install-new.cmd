ad10186402ff49b2cfa277b272a9153b8c654523
Arkhipov Elias <goodwin.v22@gmail.com>
2014-12-02 15:57:23 +0300

    version 2.0 alpha
    new version of installation

---
+++ scw-install-new.cmd
@@ -0,0 +1,102 @@
+@echo off
+:: install cygwin and scduply by Consult-MIT
+setlocal ENABLEEXTENSIONS EnableDelayedExpansion
+
+reg.exe query "HKU\S-1-5-19">nul 2>&1
+if %errorlevel% equ 1 (
+	call :UACPrompt
+	exit /b 0
+)
+set src=%cd%
+set dst=c:\cygwin
+set asm=%dst%\usr\local\src
+set cygsetup=setup-x86.exe
+set msi_7z=7z922-x64.msi
+set http_7z=http://downloads.sourceforge.net/project/sevenzip/7-Zip/9.22/7z922.msi?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsevenzip%2Ffiles%2F7-Zip%2F9.22%2F7z922.msi
+if defined ProgramFiles(x86) (
+    set cygsetup=setup-x86_64.exe
+    set http_7z=http://downloads.sourceforge.net/project/sevenzip/7-Zip/9.22/7z922-x64.msi?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsevenzip%2Ffiles%2F7-Zip%2F9.22%2F7z922-x64.msi%
+    set msi_7z=7z922-x64.msi
+)
+for /f "tokens=*" %%a in ('call :winver') do echo %%a
+call :winver
+pause > nul 2>nul
+:: call :install_all
+exit /b 0
+
+:: ==================================================================
+:: installing all things
+:install_all
+if exist %src%/%cygsetup% ( call :cygok) else ( 
+    wget http://cygwin.com/%cygsetup% 
+    call :cygok 
+    if not exist scduply.zip wget "https://github.com/skycover/scwin/zipball/master" --no-check-certificate -O scduply.zip
+    echo  call :extract_scduply
+	call :extract_scduply
+    echo call :postinst
+    call :postinst
+)
+exit /b 0
+:: ==================================================================
+
+:: ==================================================================
+:postinst
+	chdir "%src%\scwin"
+	if not exist "%asm%" mkdir %asm%
+	copy *.tar.gz %asm%
+	copy scw-postinst.sh %asm%
+	copy sysstate.* %asm%
+	copy scdw.cmd %asm%
+	chdir %asm%
+	%dst%\bin\bash %asm%\scw-postinst.sh
+exit /b 0
+:: ==================================================================
+
+:: ==================================================================
+:extract_scduply
+echo extract
+if not exist "%programfiles%\7-zip" (
+    wget %http_7z% && %msi_7z% /quiet /norestart
+)
+cd %programfiles%\7-zip
+echo "%src%\scduply.zip"
+7z e "%src%\scduply.zip" -o"%src%\scwin" -y
+exit /b 0
+:: ==================================================================
+
+:: ==================================================================
+:winver
+:: new version of getting wininfo
+for /f "tokens=2*" %%a in ('Reg QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName^|find /i "REG"') do set winver=%%b
+for /f "tokens=2*" %%a in ('Reg QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CSDVersion^|find /i "REG"') do set winver=%winver% %%b
+if exist "%programfiles% (x86)" ( set winver=%winver% x64) else ( set winver=%winver% x32) 
+echo %winver%
+exit /b 0
+:: ==================================================================
+
+:: ==================================================================
+:: install cygwin
+:cygok
+set packages="python,gnupg,gcc,gcc-core,cyglsa,librsync-devel,librsync1,wget,vim,ncftp,openssh,cron" 
+set server="http://cygwin.mirror.constant.com" 
+chdir %src%
+mkdir cygwin
+rem Use scw-install.cmd -L to install cygwin from local directory
+%cygsetup% %1 -l %src%\cygwin -R %dst% -q -P %packages% -s %server% -d
+exit /b %errorlevel%
+:: ==================================================================
+
+:: ==================================================================
+:UACPrompt
+:: запуск самого себя от имени администратора
+mshta "vbscript:CreateObject("Shell.Application").ShellExecute("%~fs0", "", "%src%", "runas", 1) & Close()"
+exit /b
+:: ==================================================================
+
+:: ==================================================================
+:: error message
+:message
+	echo You should place ditributive to %src%
+	echo And should run scw-install.cmd just from it.
+exit /b 0
+:: ==================================================================
